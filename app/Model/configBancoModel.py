23d706961ccb3e1a156b3899ae1e4b5adedac32b
Rafael Masato Haga Costa <rmcosta194@gmail.com>
2024-11-29 22:47:25 -0300

    extrai funções auxiliares da função de do módulo que inicializa o banco de dados

--- app/Model/configBancoModel.py
+++ app/Model/configBancoModel.py
@@ -1,29 +1,23 @@
 import sqlite3
 import os
 
-def CriarBancoDados():
-    conn = None  # Inicializa a conexão como None
-    cursor = None  # Inicializa o cursor como None
-    try:    
-        # Caminho completo para o arquivo database.db dentro da pasta 'database'
-        db_path = os.path.join('app/database', 'database.db')
+DB_PATH = os.path.join("app", "database", "database.db")
 
-        # Criar a conexão com o SQLite (o arquivo será criado automaticamente se não existir)
-        conn = sqlite3.connect(db_path)
 
-        # Criar um cursor
-        cursor = conn.cursor()
-
-        # Criar a tabela de categorias primeiro
-        cursor.execute("""
+def gerar_tabela_de_categorias(cursor):
+    cursor.execute(
+        """
             CREATE TABLE IF NOT EXISTS categorias (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL UNIQUE
             )
-        """)
+        """
+    )
 
-        # Criar a tabela de Gastos depois, que depende da tabela categorias
-        cursor.execute("""
+
+def gerar_tabela_de_gastos(cursor):
+    cursor.execute(
+        """
             CREATE TABLE IF NOT EXISTS Gastos (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 data TEXT NOT NULL,
@@ -31,32 +25,43 @@ def CriarBancoDados():
                 categoria_id INTEGER,
                 FOREIGN KEY (categoria_id) REFERENCES categorias (id)
             )
-        """)
+        """
+    )
 
-        # Criar a tabela de Usuário
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Usuario (
+
+def gerar_tabela_de_usuarios(cursor):
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS Usuario (
                         CPF INTEGER PRIMARY KEY AUTOINCREMENT,  
                         nome TEXT NOT NULL, 
                         idade INTEGER NOT NULL, 
                         email TEXT UNIQUE NOT NULL,
-                        senha TEXT NOT NULL)''')
+                        senha TEXT NOT NULL)"""
+    )
 
-        # Confirmar as alterações
+
+def CriarBancoDados():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+
+        gerar_tabela_de_categorias(cursor)
+        gerar_tabela_de_gastos(cursor)
+        gerar_tabela_de_usuarios(cursor)
+
         conn.commit()
         return "Tabelas setadas com sucesso"
     except Exception as e:
-        if conn:  # Verifica se a conexão foi criada antes de tentar fazer rollback
+        if conn:
             conn.rollback()
         return f"Erro ao setar tabelas: {str(e)}"
     finally:
-        if cursor:  # Fecha o cursor apenas se ele foi criado
+        if cursor:
             cursor.close()
-        if conn:  # Fecha a conexão apenas se ela foi criada
+        if conn:
             conn.close()
 
 
 def conexao():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DB_PATH = os.path.join(BASE_DIR, '../database', 'database.db')
     con = sqlite3.connect(DB_PATH)
     return con
