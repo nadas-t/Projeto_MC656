92e573dfb3ace7f91b0db764162e9ed82d1c233c
Thiago Nadim <nadimthiago@gmail.com>
2024-12-03 00:11:20 -0300

    arrumar testes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.Controller.configBanco import Cria
 # Cria a instância da aplicação Flask
 app = Flask(__name__, template_folder='View')
 app.debug = True  # Ativa o modo de depuração
-app.run()
+
 
 # Configurações da aplicação
 app.config['SECRET_KEY'] = "Ro5&B+agkatMy9cG1fLeq@Sfvn607by4" #chave para oroteger os dados
@@ -14,3 +14,6 @@ Cria()
 
 # Importa as rotas (deve ser feito depois da configuração da app)
 from app import routes
+
+if __name__ == "__main__":
+    app.run()
